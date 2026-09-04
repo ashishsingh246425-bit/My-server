b15796c2e68b8ddfc60df118402b9513b7c6b830
ashishsingh246425-bit <ashishsingh246425@gmail.com>
2026-09-04 19:18:07 +0530

    Create MainActivity.java

---
+++ android/app/src/main/java/com/studyhub/geniusquiz/MainActivity.java
@@ -0,0 +1,29 @@
+package com.studyhub.geniusquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.loadUrl(
+            "https://ashishsingh246425-bit.github.io/My-server/"
+        );
+
+        setContentView(webView);
+    }
+}
